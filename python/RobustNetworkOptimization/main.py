4fde9b43a50690a44de9c432d245367b09fa3802
Edielson P. Frigieri <edielsonpf@gmail.com>
2015-12-02 09:51:25 -0600

    Adding python project

---
+++ python/RobustNetworkOptimization/main.py
@@ -0,0 +1,88 @@
+__author__ = """Aric Hagberg (hagberg@lanl.gov)"""
+try:
+    import matplotlib.pyplot as plt
+except:
+    raise
+import networkx as nx
+from optimizer.backup import Backup
+import math
+from gurobipy import tuplelist
+
+if __name__ == '__main__':
+    pass
+
+# Constant definition
+p = 0.025
+invstd = 2.326347874
+
+#Generating graphs
+G=nx.DiGraph()
+
+G.add_edge('1','2',capcity=1)
+G.add_edge('1','3',capcity=1)
+G.add_edge('1','4',capcity=1)
+G.add_edge('2','1',capcity=1)
+G.add_edge('2','3',capcity=1)
+G.add_edge('2','4',capcity=1)
+G.add_edge('3','1',capcity=1)
+G.add_edge('3','2',capcity=1)
+G.add_edge('3','4',capcity=1)
+G.add_edge('4','1',capcity=1)
+G.add_edge('4','2',capcity=1)
+G.add_edge('4','3',capcity=1)
+
+links = G.edges()
+nodes = G.nodes() 
+
+capacity={}
+mean={}
+std={}
+for i,j in links:
+    #generating capacity list
+    capacity[i,j] = 1
+    #generating mean list
+    mean[i,j] = p
+    std[i,j]=math.sqrt(p*(1-p))
+
+elarge=[(u,v) for (u,v,d) in G.edges(data=True) if d['capcity'] >0.5]
+esmall=[(u,v) for (u,v,d) in G.edges(data=True) if d['capcity'] <=0.5]
+
+pos=nx.spring_layout(G) # positions for all nodes
+
+# nodes
+nx.draw_networkx_nodes(G,pos,node_size=500)
+
+# edges
+nx.draw_networkx_edges(G,pos,edgelist=elarge,width=2)
+nx.draw_networkx_edges(G,pos,edgelist=esmall,width=2,alpha=0.5,edge_color='b',style='dashed')
+
+# labels
+nx.draw_networkx_labels(G,pos,font_size=20,font_family='sans-serif')
+
+plt.axis('off')
+plt.savefig("weighted_graph.png") # save as png
+plt.show() # display
+
+#optimization
+links = tuplelist(links)
+BackupNet = Backup(nodes,links,capacity,mean,std,invstd)
+solution = BackupNet.optimize()
+
+#penalizes the links chosen to be backup links
+for i,j in links:
+    if solution[i,j] < 0.1:
+        G.remove_edge(i, j)
+    
+esmall=[(u,v) for (u,v) in G.edges()]
+
+# nodes
+nx.draw_networkx_nodes(G,pos,node_size=500)
+
+# edges
+nx.draw_networkx_edges(G,pos,edgelist=esmall,width=2,alpha=0.5,edge_color='b',style='dashed')
+
+# labels
+nx.draw_networkx_labels(G,pos,font_size=20,font_family='sans-serif')
+
+plt.axis('off')
+plt.show() # display
---
+++ python/RobustNetworkOptimization/optimizer/backup.py
@@ -0,0 +1,134 @@
+'''
+Created on Nov 23, 2015
+
+@author: Edielson
+'''
+import math
+from gurobipy import *
+
+class Backup(object):
+    '''
+    classdocs
+    '''
+    # Private model object
+    __model = []
+    
+    # Private model variables
+    __BackupCapacity = {}
+    __bBackupLink = {}
+    __ValidLink = {}
+        
+    # Private model parameters
+    __links = []
+    __cost = []
+    __nodes = []
+    __capacity = []
+    __mean = []
+    __std = []
+    __invstd = 1
+    
+    def __init__(self,nodes,links,capacity,mean,std,invstd):
+        '''
+        Constructor
+        '''
+        self.__links = links
+        self.__nodes = nodes
+        self.__capacity = capacity
+        self.__mean = mean
+        self.__std = std
+        self.__invstd = invstd
+        
+        self.__loadModel()
+                
+    def __loadModel(self):
+                
+        # Create optimization model
+        self.__model = Model('Backup')
+    
+        # Auxiliary variables for SOCP reformulation
+        U = {}
+        R = {}
+      
+        # Create variables
+        for i,j in self.__links:
+            self.__BackupCapacity[i,j] = self.__model.addVar(lb=0, obj=1, name='Backup_Capacity[%s,%s]' % (i, j))
+        self.__model.update()
+         
+        for i,j in self.__links:
+            for s,d in self.__links:
+                self.__bBackupLink[i,j,s,d] = self.__model.addVar(vtype=GRB.BINARY,obj=1,name='Backup_Link[%s,%s,%s,%s]' % (i, j, s, d))
+        self.__model.update()
+        
+        for i,j in self.__links:
+            U[i,j] = self.__model.addVar(obj=1,name='U[%s,%s]' % (i, j))
+        self.__model.update()
+        
+        for i,j in self.__links:
+            for s,d in self.__links:
+                R[i,j,s,d] = self.__model.addVar(obj=1,name='R[%s,%s,%s,%s]' % (i,j,s,d))
+        self.__model.update()
+        
+        self.__model.modelSense = GRB.MINIMIZE
+        #m.setObjective(quicksum([fixedCosts[p]*open[p] for p in plants]))
+        self.__model.setObjective(quicksum(self.__BackupCapacity[i,j] for i,j in self.__links))
+        self.__model.update()
+        
+           
+        #------------------------------------------------------------------------#
+        #                    Constraints definition                              #
+        #                                                                        #
+        #                                                                        #
+        #------------------------------------------------------------------------#
+         
+        # Link capacity constraints
+        for i,j in self.__links:
+            self.__model.addConstr(self.__BackupCapacity[i,j] >= quicksum(self.__mean[s,d]*self.__bBackupLink[i,j,s,d] for (s,d) in self.__links) + U[i,j]*self.__invstd,'[CONST]Link_Cap_%s_%s' % (i, j))
+        self.__model.update()
+            
+        # SCOP Reformulation Constraints
+        for i,j in self.__links:
+            self.__model.addConstr(quicksum(R[i,j,s,d]*R[i,j,s,d] for (s,d) in self.__links) <= U[i,j]*U[i,j],'[CONST]SCOP1[%s][%s]' % (i, j))
+        self.__model.update()
+            
+        # SCOP Reformulation Constraints    
+        for i,j in self.__links:
+            for s,d in self.__links:
+                self.__model.addConstr(self.__std[s,d]*self.__bBackupLink[i,j,s,d] == R[i,j,s,d],'[CONST]SCOP2[%s][%s][%s][%s]' % (i, j,s,d))
+        self.__model.update()
+        
+        for i in self.__nodes:
+            for s,d in self.__links:
+                # Flow conservation constraints
+                if i == s:
+                    self.__model.addConstr(quicksum(self.__bBackupLink[i,j,s,d] for i,j in self.__links.select(i,'*')) - 
+                                           quicksum(self.__bBackupLink[j,i,s,d] for j,i in self.__links.select('*',i)) == 1,'Flow1[%s,%s,%s,%s]' % (i,j,s, d))
+                # Flow conservation constraints
+                elif i == d:
+                    self.__model.addConstr(quicksum(self.__bBackupLink[i,j,s,d] for i,j in self.__links.select(i,'*')) - 
+                                           quicksum(self.__bBackupLink[j,i,s,d] for j,i in self.__links.select('*',i)) == -1,'Flow2[%s,%s,%s,%s]' % (i,j,s, d))
+                # Flow conservation constraints
+                else:    
+                    self.__model.addConstr(quicksum(self.__bBackupLink[i,j,s,d] for i,j in self.__links.select(i,'*')) - 
+                                           quicksum(self.__bBackupLink[j,i,s,d] for j,i in self.__links.select('*',i)) == 0,'Flow3[%s,%s,%s,%s]' % (i,j,s, d))
+        self.__model.update()
+                
+        
+    def optimize(self):
+        
+        self.__model.write('backup.lp')
+ 
+        # Compute optimal solution
+        self.__model.optimize()
+        
+        # Print solution
+        if self.__model.status == GRB.Status.OPTIMAL:
+            solution = self.__model.getAttr('x', self.__BackupCapacity)
+            for i,j in self.__links:
+                if solution[i,j] > 0:
+                    print('%s -> %s: %g' % (i, j, solution[i,j]))
+            
+        else:
+            print('Optimal value not found!\n')
+            solution = []
+            
+        return solution;    
