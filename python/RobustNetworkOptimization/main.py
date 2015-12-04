5b10deed390cd60e8738067f97ec52ffa4565b33
Edielson P. Frigieri <edielsonpf@gmail.com>
2015-12-04 13:45:24 -0600

    Writing comments to the code

--- python/RobustNetworkOptimization/main.py
+++ python/RobustNetworkOptimization/main.py
@@ -1,12 +1,12 @@
 __author__ = "Edielson"
 
-from BackupTest import BackupModelTest, BackupPathModelTest
+from BackupTest import BackupPathModelTest
 
 if __name__ == '__main__':
     pass
 
 # Constant definition
-NumNodes = 5
+NumNodes = 4
 p = 0.025
 invstd = 2.326347874
   
