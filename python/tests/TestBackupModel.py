a220c077302a4a3dcd1cb148a91b5e6b720d9663
Edielson P. Frigieri <edielsonpf@gmail.com>
2016-06-09 23:50:51 -0500

    Changing functions names

--- python/tests/TestBackupModel.py
+++ python/tests/TestBackupModel.py
@@ -1,6 +1,8 @@
 import unittest
 from NetworkOptimization.BFPBackupModel import *
+from NetworkOptimization.RndScenariosGen import *
 from NetworkOptimization.Tools import *
+from datetime import datetime
 
 class TestBackupModel(unittest.TestCase):
 
@@ -10,31 +12,35 @@ class TestBackupModel(unittest.TestCase):
         p2=2*p
         epsilon=0.01
         num_scenarios = 100
+        num_nodes=5
          
-        G,links,nodes,num_links=GetFullConnectedNetwork(5)
+        G,links,nodes,num_links=GetFullConnectedNetwork(num_nodes)
         #You must change here if there is a different capacity for each link 
         cap_per_link=[1 for i in range(num_links)]
         #Adding the respective capacity for each link in the graph
         for s,d in links:
             G.add_weighted_edges_from([(s,d,cap_per_link[links.index((s,d))])])
         
-        unif_scenarios = GetUniformRandScenarios(None, num_scenarios, num_links)
-        scenarios = GetRandScenariosFromUnif(unif_scenarios, p2, num_scenarios, num_links, links, cap_per_link)
+        ScenariosGen = ScenariosGenerator()
+        
+        unif_scenarios = ScenariosGen.GetUniformRand(None, num_scenarios, num_links)
+        scenarios = ScenariosGen.GetBinomialFromUnif(unif_scenarios, p2, num_scenarios, num_links, links, cap_per_link)
             
         #Generates the importance sampling factor for each sample
-        gamma=GetImportanceSamplingVector(links, scenarios, num_scenarios, p, p2)
+        gamma=ScenariosGen.GetImportanceSamplingVector(links, scenarios, num_scenarios, p, p2)
         
-        BackupNet = BFPBackup()
-        BackupNet.loadModel(gamma,nodes,links,scenarios,epsilon,num_scenarios)
+        BackupNet = BFPBackupNetwork()
+        BackupNet.LoadModel(gamma,nodes,links,scenarios,epsilon,num_scenarios)
         
-        OptCapacity,BackupLinks,BkpLinks = BackupNet.optimize()
+        OptCapacity,BackupLinks,BkpLinks = BackupNet.Optimize()
         
-        BackupNet.save('TestBackupNet.dat')
+        file_name=('%s,%s,%s,%s,%s,%s.dat'%(num_nodes,p,p2,epsilon,num_scenarios,datetime.strftime(datetime.now(), '%Y-%m-%d,%H-%M-%S')))
+        BackupNet.Save(file_name)
     
         #Reseting model
-        BackupNet.reset()
+        BackupNet.ResetModel()
     
-        OptCapacityLoad,BackupLinksLoad,BkpLinksLoad = LoadBackupNetwork('TestBackupNet.dat')
+        OptCapacityLoad,BackupLinksLoad,BkpLinksLoad = BackupNet.LoadBackupNetwork(file_name)
     
          
         self.assertEqual(OptCapacity, OptCapacityLoad)
@@ -49,9 +55,6 @@ class TestBackupModel(unittest.TestCase):
                 break
                   
         self.assertTrue(Answer, None)
-
-        
-    
 
 if __name__ == '__main__':
     unittest.main()
