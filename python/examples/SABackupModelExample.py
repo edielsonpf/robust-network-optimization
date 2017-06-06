cf0a46755bb5d6afc5292a3efae7a38cf64f38d5
Edielson P. Frigieri <edielsonpf@gmail.com>
2017-06-05 22:01:36 -0300

    Removing SA model

--- python/examples/SABackupModelExample.py
+++
@@ -1,9 +0,0 @@
-from NetworkOptimization.SA_Model import *
-
-
-label = 'test';
-datafile = '../examples/nsfnetFail.dat';
-probLinkFail = 0.0005;
-epsilon = 1e-5;
-
-design(datafile, label,probLinkFail,epsilon)
